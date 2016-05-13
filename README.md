e8751031f985f84eb81282677009e19798eac3b5
Eric Cadow <eric@cadow.net>
2016-05-12 20:43:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# hapi-sequelize-rest
+
+RESTful Hapi routes based on sequelize mdoels
+
+
+## Install
+
+```bash
+$ npm install hapi-sequelize-rest
+```
+
+
+## Usage
+
+```bash
+$ echo "details coming soon"
+```
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const RouteBuilder = require('./lib/routeBuilder');
+
+const defaultOptions = {
+    prefix: '/',
+    readOnly: false
+};
+
+exports.register = (server, options, next) => {
+
+    const models = server.plugins['hapi-sequelize'].db.sequelize.models;
+
+    Object.keys(models).filter((modelName) => {
+
+        return modelName.toLowerCase() === models[modelName].options.name.singular;
+    }).forEach((modelName) => {
+
+        const model = models[modelName];
+
+        RouteBuilder.list(server, options, model);
+        RouteBuilder.create(server, options, model);
+        RouteBuilder.get(server, options, model);
+        RouteBuilder.update(server, options, model);
+        RouteBuilder.delete(server, options, model);
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
